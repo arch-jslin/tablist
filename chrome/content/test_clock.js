a031b822d48bd7b47f2fd229746b4fc573258f09
Archilife Lin <archilifelin@yahoo.com.tw>
2010-08-31 09:04:48 +0800

    updated initTime

--- chrome/content/test_clock.js
+++ chrome/content/test_clock.js
@@ -1,7 +1,9 @@
-function initTime() {
-  alert("HI!");
+function initTime(event) {
   updateTime();
   window.setInterval(updateTime, 1000);
+  var myPanel = document.getElementById("my-panel");
+  alert(event.target);
+  myPanel.openPopup( myPanel , "before_start" , 0 , 0 , false, false);
 }
 
 function updateTime() {
